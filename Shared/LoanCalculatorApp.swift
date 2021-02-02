2a562132795274e994591edda27ee42a29b75364
Gagandeep Singh <gagan.deep@me.com>
2021-02-02 21:45:47 +1100

    Initial Commit

---
+++ Shared/LoanCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanCalculatorApp.swift
+//  Shared
+//
+//  Created by Gagandeep Singh on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LoanCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
